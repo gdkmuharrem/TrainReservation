ad7409a45142b96fb0298ea1c88619ad58779757
Muharrem GEDİK <39222515+gdkmuharrem@users.noreply.github.com>
2025-07-15 16:55:32 +0300

    Update Program.cs

--- TrainReservation.API/Program.cs
+++ TrainReservation.API/Program.cs
@@ -28,11 +28,11 @@ app.Urls.Add($"http://*:{port}");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    
     app.UseHttpsRedirection();
 }
-
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("AllowAll");
 app.UseAuthorization();
