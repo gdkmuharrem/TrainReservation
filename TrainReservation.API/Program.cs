4dccd7ae202e4c71fe2dfcac1c54184603538068
Muharrem GEDİK <39222515+gdkmuharrem@users.noreply.github.com>
2025-07-15 16:44:09 +0300

    Update Program.cs

--- TrainReservation.API/Program.cs
+++ TrainReservation.API/Program.cs
@@ -22,6 +22,9 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Urls.Add($"http://*:{port}");
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
